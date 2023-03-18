d5ca1e12f0c3ecee7c438b963651e0e63fffb879
Azharrmdnn2 <azhar118428@gmail.com>
2023-03-19 01:52:28 +0700

    Model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cart extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'users_id', 'products_id'
+    ];
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
--- app/Models/TransactionItem.php
+++ app/Models/TransactionItem.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransactionItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'users_id', 'products_id', 'transactions_id'
+    ];
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
 }
