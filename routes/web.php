d4b93de77995e7a93b657b2d5d901a75809b0364
Azharrmdnn2 <azhar118428@gmail.com>
2023-03-19 02:29:09 +0700

    Blade Template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index'])
+    ->name('index');
+Route::get('/details/{slug}', [FrontendController::class, 'details'])
+    ->name('details');
+Route::get('/cart', [FrontendController::class, 'cart'])
+    ->name('cart');
+Route::get('/checkout/success', [FrontendController::class, 'success'])
+    ->name('checkout-success');
 
 Route::middleware([
     'auth:sanctum',
@@ -26,3 +32,16 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+
+
+// Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function () {
+//     Route::get('/', [DashboardController::class, 'index'])->name('index');
+//     Route::middleware(['admin'])->group(function () {
+//         Route::resource('product', ProductController::class);
+//         Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
+//             'index', 'create', 'store', 'destroy'
+//         ]);
+//     });
+// });
